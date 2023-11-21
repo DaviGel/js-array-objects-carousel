f2b8a4fb79336a29423c121a93f96deb48f310d5
DaviGel <geloso.davide@gmail.com>
2023-11-21 17:44:29 +0100

    feat: milestone 1 in progress

--- js/main.js
+++ js/main.js
@@ -32,63 +32,77 @@ const items = document.querySelector('.items');
 let counter = 0;
 let item = '';
 let thumbnail = '';
-const caption = [
+const fragment = new DocumentFragment();
+const images = [
   {
     url: 'img/01.jpg',
-    titolo: 'test01',
+    titolo: 'Title 01',
     descrizione:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nisi odit ab aspernatur.',
   },
   {
     url: 'img/02.jpg',
-    titolo: 'test02',
+    titolo: 'Title 02',
     descrizione:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas aliquam fugiat tenetur dignissimos labore ratione delectus?',
   },
   {
     url: 'img/03.jpg',
-    titolo: 'test03',
+    titolo: 'Title 03',
     descrizione:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti, hic nesciunt, consequatur.',
   },
   {
     url: 'img/04.jpg',
-    titolo: 'test04',
+    titolo: 'Title 04',
     descrizione:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus doloribus exercitationem.',
   },
   {
     url: 'img/05.jpg',
-    titolo: 'test05',
+    titolo: 'Title 05',
     descrizione:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, porro eum ex nobis beatae blanditiis iure.',
   },
 ];
 
-// Execution
+// Program
+// Aggiungione immagini tramite interpolazione di stringhe
 for (let i = 0; i < imagesArray.length; i++) {
   if (i === 0) {
-    item += `<div class = "item active"><img src = "img/${imagesArray[i]}" alt = "img${i}"></div>`;
-    thumbnail += `<div class = "thumbnail"><img src = "img/${imagesArray[i]}" alt = "img${i}"></div>`;
+    item += `<div class = "item active"><img src = "${
+      images[i].url
+    }" alt = "img${i + 1}"><div class = "caption"><h2>${
+      images[i].titolo
+    }</h2><br><span>${images[i].descrizione}</span></div></div>`;
+    thumbnail += `<div class = "thumbnail"><img src = "${
+      images[i].url
+    }" alt = "img${i + 1}"></div>`;
   } else {
-    item += `<div class = "item"><img src = "img/${imagesArray[i]}" alt = "img${i}"></div>`;
-    thumbnail += `<div class = "thumbnail overlay"><img src = "img/${imagesArray[i]}" alt = "img${i}"></div>`;
+    item += `<div class = "item"><img src = "${images[i].url}" alt = "img${
+      i + 1
+    }"><div class = "caption"><h2>${images[i].titolo}</h2><br><span>${
+      images[i].descrizione
+    }</span></div></div>`;
+    thumbnail += `<div class = "thumbnail overlay"><img src = "${images[i].url}" alt = "img${i}"></div>`;
   }
 }
 
 items.innerHTML +=
   item + `<div class = "thumbnailsContainer">${thumbnail}</div>`;
 
+// Variables
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
 
 const domItem = document.querySelectorAll('.item');
 const domThumbnails = document.querySelectorAll('.thumbnail');
 const thumbnailsContainer = document.querySelector('.thumbnailsContainer');
+
 const play = document.querySelector('.start i');
 const pause = document.querySelector('.stop i');
-const fragment = new DocumentFragment();
 
+// Program
 // let myInterval = setInterval(changeSlideNext, 3_000);
 let myInterval;
 
@@ -109,9 +123,12 @@ pause.addEventListener('click', function () {
   clearInterval(myInterval);
 });
 
+// Aggiungo l'EL al padre per gestire i click ai figli passando il parametro event alla funzione anonima
 thumbnailsContainer.addEventListener('click', function (event) {
+  console.log(domItem[0].childNodes[0].alt);
+  console.log(event);
   for (let i = 0; i < domItem.length; i++)
-    if (event.target.innerHTML === domItem[i].innerHTML) {
+    if (event.target.innerHTML === domItem[i].childNodes[i].alt) {
       domItem[counter].classList.remove('active');
       domThumbnails[counter].classList.add('overlay');
       counter = i;
